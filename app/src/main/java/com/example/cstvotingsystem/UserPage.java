06e6f90ba9965228db3ad6ca5b022fc4d8d8b1f0
choden607 <02200169.cst@rub.edu.bt>
2022-10-10 22:10:56 +0600

    thinley's edit

--- app/src/main/java/com/example/cstvotingsystem/UserPage.java
+++ app/src/main/java/com/example/cstvotingsystem/UserPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,5 +32,11 @@ public class UserPage extends AppCompatActivity {
     public void candidate(View view) {
         startActivity(new Intent(getApplicationContext(), ViewCandidates.class));
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
