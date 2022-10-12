948c4140dff3981af4d36edea7f0c34cc48f7731
choden607 <02200169.cst@rub.edu.bt>
2022-10-12 23:09:31 +0600

    menu done

--- app/src/main/java/com/example/cstvotingsystem/AboutPage.java
+++ app/src/main/java/com/example/cstvotingsystem/AboutPage.java
@@ -11,5 +11,10 @@ public class AboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 }
--- app/src/main/java/com/example/cstvotingsystem/UserPage.java
+++ app/src/main/java/com/example/cstvotingsystem/UserPage.java
@@ -1,11 +1,18 @@
 package com.example.cstvotingsystem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserPage extends AppCompatActivity {
 
@@ -15,6 +22,10 @@ public class UserPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         vote = findViewById(R.id.vote);
         result = findViewById(R.id.result);
@@ -26,44 +37,46 @@ public class UserPage extends AppCompatActivity {
 
             }
         });
-
-
-
-}
-
-    public void candidate(View view) {
-        startActivity(new Intent(getApplicationContext(), ViewCandidates.class));
-
     }
 
-    public void Result(View view) {
-        startActivity(new Intent(getApplicationContext(), UserProfile.class));
+        public void candidate (View view){
+            startActivity(new Intent(getApplicationContext(), ViewCandidates.class));
 
-    }
-
-   /* public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                this.finish();
-                return true;
         }
-        if(item.getItemId() == R.id.logout){
+
+        public boolean onCreateOptionsMenu (Menu menu){
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+        @Override
+        public boolean onOptionsItemSelected (@NonNull MenuItem item){
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    this.finish();
+                    return true;
+            }
+            if (item.getItemId() == R.id.logout) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+            if (item.getItemId() == R.id.user_profile) {
+                startActivity(new Intent(getApplicationContext(), UserProfile.class));
+            }
+            if (item.getItemId() == R.id.about) {
+                startActivity(new Intent(getApplicationContext(), AboutPage.class));
+            }
+            return super.onOptionsItemSelected(item);
+        }
+        public void logout_user (View view){
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(getApplicationContext(), Login.class));
             finish();
         }
-        if(item.getItemId() == R.id.user_profile){
-            startActivity(new Intent(getApplicationContext(), UserPage.class));
+
+
+        public void gotocandidate (View view){
+            startActivity(new Intent(getApplicationContext(), ViewCandidates.class));
         }
-        if(item.getItemId() == R.id.about){
-            startActivity(new Intent(getApplicationContext(), AboutPage.class));
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-}
+    }
