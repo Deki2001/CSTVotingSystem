386f4f7c72c7cd67773bcc7905b91d1c9a95aa45
Kunzangg <kuenzangd485@gmail.com>
2022-10-19 11:00:43 -0700

    Edited slider

--- app/src/main/java/com/example/cstvotingsystem/ViewResult.java
+++ app/src/main/java/com/example/cstvotingsystem/ViewResult.java
@@ -93,4 +93,5 @@ public class ViewResult extends AppCompatActivity {
     }
 
 
+
 }
--- app/src/main/java/com/example/cstvotingsystem/Vote.java
+++ app/src/main/java/com/example/cstvotingsystem/Vote.java
@@ -2,9 +2,16 @@ package com.example.cstvotingsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Vote extends AppCompatActivity {
 
@@ -12,7 +19,41 @@ public class Vote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout){
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), Login.class));
+            finish();
+        }
+        if(item.getItemId() == R.id.user_profile){
+            startActivity(new Intent(getApplicationContext(), UserProfile.class));
+        }
+        if(item.getItemId() == R.id.about){
+            startActivity(new Intent(getApplicationContext(), AboutPage.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void logout_user (View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+
+
 
     public void ccresult(View view) {
         startActivity(new Intent(getApplicationContext(), ViewResult.class));
